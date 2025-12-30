197fe84850615f528a182a62f8aa62c0ac8da2c7
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 18:23:05 +0530

    Add chapter data for numbers and Roman numerals

--- data/maths/class5/c5_ch1_numbers.js
+++ data/maths/class5/c5_ch1_numbers.js
@@ -1,1 +1,79 @@
 
+window.currentChapterData = {
+    "Numbers to 10 Crores": {
+        "Easy": [
+            { q: "How many zeros are in 1 Crore?", a: "7", opts: ["5", "6", "7", "8"] },
+            { q: "What is the place value of 5 in 54,00,000?", a: "Fifty Lakh", opts: ["Five Lakh", "Fifty Lakh", "Five Crore", "Fifty Crore"] },
+            { q: "Smallest 7-digit number?", a: "10,00,000", opts: ["1,00,000", "10,00,000", "99,99,999", "1,00,00,000"] },
+            { q: "100 Lakhs is equal to ____.", a: "1 Crore", opts: ["10 Lakh", "1 Crore", "10 Crore", "1 Million"] },
+            { q: "Successor of 99,99,999?", a: "1,00,00,000", opts: ["10,00,000", "1,00,00,000", "99,99,998", "1 Crore 1"] },
+            { q: "Where does the first comma go in 12345678 (Indian System)?", a: "1,23,45,678", opts: ["12,345,678", "1,23,45,678", "123,45,678", "None"] },
+            { q: "Write in figures: Two Crore five lakh.", a: "2,05,00,000", opts: ["2,50,00,000", "2,05,00,000", "2,00,05,000", "25,00,000"] },
+            { q: "Face value of 8 in 8,12,34,567?", a: "8", opts: ["8", "8 Crore", "80 Lakh", "0"] },
+            { q: "How many thousands make a Lakh?", a: "100", opts: ["10", "100", "1000", "1"] },
+            { q: "Predecessor of 1 Crore?", a: "99,99,999", opts: ["99,99,999", "1,00,00,001", "9,99,999", "90,00,000"] }
+        ],
+        "Moderate": [
+            { q: "1 million is equal to how many lakhs?", a: "10", opts: ["1", "10", "100", "0.1"] },
+            { q: "Sum of place values of 2 in 24,20,500?", a: "20,20,000", opts: ["2,02,000", "20,20,000", "22,00,000", "40,000"] },
+            { q: "Difference between 1 Crore and 1 Lakh?", a: "99,00,000", opts: ["90,00,000", "99,00,000", "1,00,000", "9,99,000"] },
+            { q: "Write 45,000,000 in Indian System.", a: "4 Crore 50 Lakh", opts: ["45 Lakh", "4 Crore 5 Lakh", "4 Crore 50 Lakh", "45 Crore"] },
+            { q: "How many millions make 1 Crore?", a: "10", opts: ["1", "10", "100", "1000"] },
+            { q: "Which is greater: 10,00,000 or 1,00,00,000?", a: "1,00,00,000", opts: ["10,00,000", "1,00,00,000", "Equal", "None"] },
+            { q: "Place value of 0 in 7,05,43,210?", a: "0", opts: ["0", "Lakh", "Ten Lakh", "None"] },
+            { q: "Expanded form of 2,15,00,000?", a: "2Cr + 10L + 5L", opts: ["2Cr + 15L", "2Cr + 10L + 5L", "20L + 15L", "None"] },
+            { q: "How many zeros in 10 Crores?", a: "8", opts: ["7", "8", "9", "6"] },
+            { q: "Successor of 4,59,99,999?", a: "4,60,00,000", opts: ["4,59,99,998", "4,60,00,000", "5,00,00,000", "4,50,00,000"] }
+        ],
+        "Tough": [
+            { q: "Difference between the smallest 8-digit and largest 7-digit number?", a: "1", opts: ["0", "1", "10", "100"] },
+            { q: "How many lakhs are in 5 crores?", a: "500", opts: ["50", "500", "5000", "5"] },
+            { q: "Number of 7-digit numbers in all?", a: "90,00,000", opts: ["10,00,000", "90,00,000", "99,99,999", "1,00,00,000"] },
+            { q: "Smallest 8-digit number using 0, 1, 2 only?", a: "1,00,00,002", opts: ["1,00,00,012", "1,00,00,002", "1,11,11,112", "2,00,00,001"] },
+            { q: "Value of (5 Crore + 50 Lakh + 500)?", a: "5,50,00,500", opts: ["5,05,00,500", "5,50,00,500", "5,50,50,000", "5,00,50,500"] },
+            { q: "Place value of 4 in 4,80,00,000 is how many times its face value?", a: "1,00,00,000", opts: ["10,00,000", "1,00,00,000", "4,00,00,000", "1"] },
+            { q: "What is 10 Crore minus 1?", a: "9,99,99,999", opts: ["9,99,99,999", "9,00,00,000", "10,00,00,001", "8,99,99,999"] },
+            { q: "Number of Ten-Thousands in 2 Crores?", a: "2,000", opts: ["200", "2,000", "20,000", "2,00,000"] },
+            { q: "If a=1Cr and b=1L, what is a/b?", a: "100", opts: ["10", "100", "1000", "0.1"] },
+            { q: "Smallest 8-digit number with different digits?", a: "1,02,34,567", opts: ["1,00,00,000", "1,23,45,678", "1,02,34,567", "1,01,23,456"] }
+        ]
+    },
+    "Roman Numerals": {
+        "Easy": [
+            { q: "What is V in Hindu-Arabic?", a: "5", opts: ["1", "5", "10", "50"] },
+            { q: "What is X in Hindu-Arabic?", a: "10", opts: ["5", "10", "50", "100"] },
+            { q: "Roman numeral for 100?", a: "C", opts: ["L", "C", "D", "M"] },
+            { q: "Roman numeral for 50?", a: "L", opts: ["X", "L", "C", "D"] },
+            { q: "What is III?", a: "3", opts: ["1", "2", "3", "4"] },
+            { q: "What is IX?", a: "9", opts: ["9", "11", "4", "6"] },
+            { q: "Roman numeral for 4?", a: "IV", opts: ["IIII", "IV", "VI", "V"] },
+            { q: "What is XXI?", a: "21", opts: ["19", "21", "12", "20"] },
+            { q: "What is XV?", a: "15", opts: ["10", "15", "14", "16"] },
+            { q: "Roman numeral for 1000?", a: "M", opts: ["C", "D", "M", "K"] }
+        ],
+        "Moderate": [
+            { q: "What is XL?", a: "40", opts: ["40", "60", "50", "10"] },
+            { q: "What is XC?", a: "90", opts: ["80", "90", "100", "110"] },
+            { q: "Roman numeral for 75?", a: "LXXV", opts: ["LXV", "LXXV", "XXL", "VXXL"] },
+            { q: "What is CD?", a: "400", opts: ["400", "600", "500", "100"] },
+            { q: "Roman numeral for 39?", a: "XXXIX", opts: ["XXIX", "XXXIX", "XL", "IXXX"] },
+            { q: "What is LXVI?", a: "66", opts: ["46", "56", "66", "76"] },
+            { q: "Roman numeral for 500?", a: "D", opts: ["C", "D", "M", "L"] },
+            { q: "What is CM?", a: "900", opts: ["900", "1100", "1000", "800"] },
+            { q: "Can 'V' be repeated in Roman Numerals?", a: "No", opts: ["Yes", "No", "Maybe", "Only twice"] },
+            { q: "Maximum times 'X' can be repeated?", a: "3", opts: ["2", "3", "4", "Unlimited"] }
+        ],
+        "Tough": [
+            { q: "Roman numeral for 1999?", a: "MCMXCIX", opts: ["MIM", "MCMXCIX", "MXMIX", "MDCCCCXCIX"] },
+            { q: "What is CMLXXXIV?", a: "984", opts: ["884", "984", "1084", "964"] },
+            { q: "Roman numeral for 444?", a: "CDXLIV", opts: ["CCCCXLIV", "CDXLIV", "DCXLIV", "CDLIV"] },
+            { q: "What is DCCL?", a: "750", opts: ["650", "750", "850", "550"] },
+            { q: "Solve: LX + XL", a: "100 (C)", opts: ["XC", "C", "CX", "LXL"] },
+            { q: "Roman numeral for 2025?", a: "MMXXV", opts: ["MMXXV", "MMXV", "MXXV", "MMXX"] },
+            { q: "What is MMM?", a: "3000", opts: ["300", "3000", "30000", "30"] },
+            { q: "Which is greater: XCIX or IC?", a: "XCIX (IC is invalid)", opts: ["XCIX", "IC", "Equal", "None"] },
+            { q: "Value of MDCLXVI?", a: "1666", opts: ["1555", "1666", "1777", "1444"] },
+            { q: "Roman numeral for 88?", a: "LXXXVIII", opts: ["LXXXVIII", "XXCIII", "LXXXIII", "XCIII"] }
+        ]
+    }
+};
