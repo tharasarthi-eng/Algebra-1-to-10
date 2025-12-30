aae84bc27903b1a9da47062ec25d9f361277a49d
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 21:02:52 +0530

    Update and rename c10_ch6_triangles.js to  c10_ch6_triangles.js

---
+++ data/maths/class10/ c10_ch6_triangles.js
@@ -0,0 +1,117 @@
+
+window.currentChapterData = {
+    "Similarity & Basic Proportionality": {
+        "Easy": [
+            { q: "Two polygons are similar if their corresponding angles are ____.", a: "Equal", opts: ["Proportional", "Equal", "Supplementary", "None"] },
+            { q: "Two polygons are similar if their corresponding sides are ____.", a: "Proportional", opts: ["Equal", "Proportional", "Parallel", "None"] },
+            { q: "Are all circles similar to each other?", a: "Yes", opts: ["Yes", "No", "Only if radii equal", "None"] },
+            { q: "Are all squares similar to each other?", a: "Yes", opts: ["Yes", "No", "Only if sides equal", "None"] },
+            { q: "Basic Proportionality Theorem is also known as ____.", a: "Thales Theorem", opts: ["Pythagoras Theorem", "Thales Theorem", "Euclid's Lemma", "None"] },
+            { q: "If a line divides any two sides of a triangle in the same ratio, it is ____ to the third side.", a: "Parallel", opts: ["Perpendicular", "Parallel", "Equal", "None"] },
+            { q: "All ____ triangles are similar.", a: "Equilateral", opts: ["Isosceles", "Scalene", "Equilateral", "Right-angled"] },
+            { q: "If ΔABC ~ ΔPQR, then AB/PQ = BC/QR = ____.", a: "AC/PR", opts: ["AC/PQ", "AC/PR", "PR/AC", "1"] },
+            { q: "The ratio of the perimeters of two similar triangles is ____ the ratio of their corresponding sides.", a: "Equal to", opts: ["Square of", "Equal to", "Half of", "None"] },
+            { q: "If DE || BC in ΔABC, then AD/DB = ____.", a: "AE/EC", opts: ["AE/AC", "AE/EC", "AB/AC", "None"] }
+        ],
+        "Moderate": [
+            { q: "In ΔABC, DE || BC. If AD=1.5cm, DB=3cm, AE=1cm, find EC.", a: "2 cm", opts: ["1.5", "2", "2.5", "3"] },
+            { q: "In ΔABC, DE || BC. If AD/DB = 3/5 and AC=4.8cm, find AE.", a: "1.8 cm", opts: ["1.8", "2.4", "3.0", "None"] },
+            { q: "A vertical pole of length 6m casts a shadow 4m long. At the same time a tower casts a shadow 28m long. Find tower height.", a: "42 m", opts: ["36", "40", "42", "48"] },
+            { q: "If the diagonals of a quadrilateral divide each other proportionally, the quadrilateral is a ____.", a: "Trapezium", opts: ["Parallelogram", "Rectangle", "Trapezium", "Rhombus"] },
+            { q: "In ΔABC, AD is the bisector of ∠A. If AB=10, AC=14, BC=6, find BD.", a: "2.5 cm", opts: ["2.5", "3.5", "3", "None"] },
+            { q: "If ΔABC ~ ΔDEF and BC=3, EF=4, and area of ΔABC=54, find area of ΔDEF.", a: "96", opts: ["72", "96", "108", "144"] },
+            { q: "CM and RN are medians of similar triangles ABC and PQR. Then ΔAMC is similar to ____.", a: "ΔPNR", opts: ["ΔPNR", "ΔQNR", "ΔPQR", "None"] },
+            { q: "The line joining the midpoints of two sides of a triangle is ____ to the third side.", a: "Parallel and half", opts: ["Parallel and equal", "Parallel and half", "Perpendicular", "None"] },
+            { q: "If ΔABC ~ ΔPQR and ∠A=45°, ∠B=75°, find ∠R.", a: "60°", opts: ["45°", "60°", "75°", "120°"] },
+            { q: "In a trapezium ABCD, AB || CD. Diagonals intersect at O. Then AO/OC = ____.", a: "BO/OD", opts: ["BO/OD", "DO/OB", "AB/CD", "None"] }
+        ],
+        "Tough": [
+            { q: "In ΔABC, DE || BC. If AD = x, DB = x-2, AE = x+2 and EC = x-1, find x.", a: "4", opts: ["2", "3", "4", "5"] },
+            { q: "If a line intersects sides AB and AC of ΔABC at D and E and is parallel to BC, then AD/AB = ____.", a: "AE/AC", opts: ["AE/AC", "AE/EC", "DB/AB", "None"] },
+            { q: "In ΔABC, ∠B = ∠C and D, E are points on AB, AC such that AD = AE. Is DE || BC?", a: "Yes", opts: ["Yes", "No", "Maybe", "Only if right-angled"] },
+            { q: "The internal bisector of an angle of a triangle divides the opposite side in the ratio of ____.", a: "The corresponding sides", opts: ["1:1", "Square of sides", "The corresponding sides", "None"] },
+            { q: "In ΔABC, D is a point on BC such that ∠ADC = ∠BAC. Then CA² = ____.", a: "CB.CD", opts: ["CB.CD", "AB.AC", "BD.CD", "None"] },
+            { q: "In a ΔABC, DE || BC. If area(ΔADE) : area(trapezium DECB) = 1:8, find AD:AB.", a: "1:3", opts: ["1:2", "1:3", "1:9", "None"] },
+            { q: "If the areas of two similar triangles are equal, then the triangles are ____.", a: "Congruent", opts: ["Similar only", "Congruent", "Equilateral", "None"] },
+            { q: "D, E, F are midpoints of sides of ΔABC. Ratio of area(ΔDEF) to area(ΔABC) is ____.", a: "1:4", opts: ["1:2", "1:4", "1:8", "None"] },
+            { q: "In ΔABC, P and Q are points on AB and AC such that PQ || BC. If AP=2.4, AB=3.2, QC=2, find AQ.", a: "6", opts: ["4", "5", "6", "8"] },
+            { q: "If the ratio of altitudes of two similar triangles is 3:5, the ratio of their areas is ____.", a: "9:25", opts: ["3:5", "9:25", "6:10", "None"] }
+        ]
+    },
+    "Criteria for Similarity (AAA, SSS, SAS)": {
+        "Easy": [
+            { q: "If in two triangles, corresponding angles are equal, the triangles are ____.", a: "Similar", opts: ["Congruent", "Similar", "Equal", "None"] },
+            { q: "AAA similarity criterion can also be called ____ criterion.", a: "AA", opts: ["SS", "AA", "AS", "None"] },
+            { q: "If corresponding sides of two triangles are in the same ratio, they are similar by ____.", a: "SSS", opts: ["SSS", "SAS", "AAA", "RHS"] },
+            { q: "SAS similarity requires one angle equal and the sides ____ that angle to be proportional.", a: "Including", opts: ["Opposite", "Including", "Adjacent to", "None"] },
+            { q: "If ΔABC ~ ΔDEF by AA, then ∠A=∠D and ____.", a: "∠B=∠E", opts: ["∠B=∠E", "AB/DE=1", "∠B=∠F", "None"] },
+            { q: "Can two triangles be similar if their sides are 3, 4, 5 and 6, 8, 10?", a: "Yes (SSS)", opts: ["Yes", "No", "Only if angles given", "None"] },
+            { q: "In similar triangles, the ratio of corresponding sides is called the ____.", a: "Scale factor", opts: ["Constant", "Scale factor", "Ratio", "Common difference"] },
+            { q: "If ΔABC ~ ΔPQR, is ∠B = ∠Q?", a: "Yes", opts: ["Yes", "No", "Sometimes", "None"] },
+            { q: "Which is NOT a similarity criterion?", a: "SSA", opts: ["AAA", "SSS", "SAS", "SSA"] },
+            { q: "If two angles of one triangle are equal to two angles of another, are they similar?", a: "Yes", opts: ["Yes", "No", "Only if sides equal", "None"] }
+        ],
+        "Moderate": [
+            { q: "In ΔABC and ΔDEF, AB/DE = BC/FD. For similarity, which angles must be equal?", a: "∠B = ∠D", opts: ["∠A = ∠D", "∠B = ∠D", "∠B = ∠E", "∠C = ∠F"] },
+            { q: "ΔABC ~ ΔPQR. If area(ΔABC)/area(ΔPQR) = 9/4 and AB=18cm, find PQ.", a: "12 cm", opts: ["10", "12", "14", "16"] },
+            { q: "Are two isosceles triangles always similar if their vertical angles are equal?", a: "Yes (AA)", opts: ["Yes", "No", "Only if base angles equal", "None"] },
+            { q: "In ΔPQR, S and T are points on PQ and PR such that ∠PST = ∠PRQ. Then ΔPST ~ ____.", a: "ΔPRQ", opts: ["ΔPQR", "ΔPRQ", "ΔTSR", "None"] },
+            { q: "If the ratio of sides of two similar triangles is 4:9, find ratio of their areas.", a: "16:81", opts: ["2:3", "4:9", "16:81", "8:18"] },
+            { q: "A girl of height 90cm is walking away from the base of a lamp-post at 1.2m/s. If lamp is 3.6m high, find shadow length after 4s.", a: "1.6 m", opts: ["1.2", "1.6", "2.0", "4.8"] },
+            { q: "Diagonal AC of a parallelogram ABCD intersects a segment AE at F (E is on BC). Then ΔADF ~ ____.", a: "ΔEBF", opts: ["ΔEBF", "ΔABC", "ΔADC", "None"] },
+            { q: "If ΔABC ~ ΔEDF and ΔABC is not similar to ΔDEF, then which is true?", a: "BC.DF = AC.EF", opts: ["BC.EF = AC.FD", "BC.DF = AC.EF", "BC.DE = AB.EF", "None"] },
+            { q: "In ΔABC, AB=6cm, BC=4cm, AC=8cm. In ΔPQR, PQ=4.5cm, QR=3cm, PR=6cm. Triangles are similar by?", a: "SSS", opts: ["SAS", "SSS", "AAA", "Not similar"] },
+            { q: "Two right triangles are always similar if ____.", a: "One acute angle is equal", opts: ["Hypotenuses equal", "One acute angle is equal", "Sides are equal", "None"] }
+        ],
+        "Tough": [
+            { q: "In ΔABC, ∠A=90° and AD ⊥ BC. Then ΔABD ~ ____.", a: "ΔCAD", opts: ["ΔACD", "ΔCAD", "ΔABC", "Both B & C"] },
+            { q: "If ΔABC ~ ΔQRP, Area(ABC)/Area(PQR) = 9/4, AB=18, BC=15, then PR = ____.", a: "10", opts: ["8", "10", "12", "None"] },
+            { q: "S is a point on side PQ of ΔPQR such that ∠PSQ = ∠PRQ? (Logic check: P,S,Q collinear). If ∠R is split, then ____.", a: "PR² = PS.PQ", opts: ["PR² = PS.PQ", "QR² = QS.QP", "Both", "None"] },
+            { q: "A tower stands vertically. A man 2m tall 10m from tower sees tip of shadow of tower and his shadow coincide. If shadow length is 4m, tower height?", a: "7 m", opts: ["5", "7", "12", "15"] },
+            { q: "If ΔABC ~ ΔDEF, then (AB+BC+CA)/(DE+EF+FD) is equal to ____.", a: "AB/DE", opts: ["(AB/DE)²", "AB/DE", "Area ratio", "None"] },
+            { q: "In ΔABC, D is midpoint of BC. AD is bisected at E. BE produced cuts AC at X. Find AF/AC.", a: "1/3", opts: ["1/2", "1/3", "1/4", "2/3"] },
+            { q: "Two similar triangles have areas 121 and 64. If median of first is 12.1, median of second is?", a: "8.8", opts: ["6.4", "8.8", "9.2", "None"] },
+            { q: "In ΔABC, DE || BC and AD:DB = 2:3. Find Area(ADE)/Area(ABC).", a: "4/25", opts: ["4/9", "4/25", "2/5", "None"] },
+            { q: "If two triangles have two sides of one proportional to two sides of another and one angle equal, are they similar?", a: "Only if angle is included", opts: ["Always", "Never", "Only if angle is included", "None"] },
+            { q: "In ΔABC, ∠B = 90°, BD ⊥ AC. If AD=4, CD=9, find BD.", a: "6", opts: ["5", "6", "6.5", "13"] }
+        ]
+    },
+    "Pythagoras Theorem (Similarity Approach)": {
+        "Easy": [
+            { q: "In a right triangle, the square of the hypotenuse is equal to the ____ of squares of other sides.", a: "Sum", opts: ["Difference", "Product", "Sum", "None"] },
+            { q: "The converse of Pythagoras Theorem helps identify if a triangle is ____.", a: "Right-angled", opts: ["Equilateral", "Isosceles", "Right-angled", "Scalene"] },
+            { q: "If sides are 3, 4, 5, is it a right triangle?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "In ΔABC, if ∠B=90°, then AC² = ____.", a: "AB² + BC²", opts: ["AB² + BC²", "AB² - BC²", "BC² - AB²", "None"] },
+            { q: "The longest side of a right triangle is the ____.", a: "Hypotenuse", opts: ["Base", "Perpendicular", "Hypotenuse", "Median"] },
+            { q: "Find the hypotenuse if sides are 6cm and 8cm.", a: "10 cm", opts: ["10", "12", "14", "100"] },
+            { q: "In a square of side 'a', the diagonal is ____.", a: "a√2", opts: ["2a", "a√2", "a²", "None"] },
+            { q: "Is (5, 12, 13) a Pythagorean triplet?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Pythagoras was a mathematician from ____.", a: "Greece", opts: ["India", "Greece", "Egypt", "China"] },
+            { q: "If a² + b² = c², then the angle opposite to side 'c' is ____.", a: "90°", opts: ["45°", "60°", "90°", "180°"] }
+        ],
+        "Moderate": [
+            { q: "Find the altitude of an equilateral triangle of side 12cm.", a: "6√3 cm", opts: ["6", "6√2", "6√3", "12"] },
+            { q: "A ladder 10m long reaches a window 8m above ground. Distance of foot from wall?", a: "6 m", opts: ["2", "6", "18", "None"] },
+            { q: "A man goes 24m West and then 10m North. How far from starting point?", a: "26 m", opts: ["14", "26", "34", "None"] },
+            { q: "In an equilateral triangle, prove that three times the square of one side is equal to four times the square of its ____.", a: "Altitude", opts: ["Median", "Altitude", "Base", "None"] },
+            { q: "Find the diagonal of a rectangle with sides 15cm and 8cm.", a: "17 cm", opts: ["17", "23", "25", "None"] },
+            { q: "In ΔABC, AB=6√3 cm, AC=12cm, BC=6cm. Find ∠B.", a: "90°", opts: ["45°", "60°", "90°", "120°"] },
+            { q: "If the diagonal of a rhombus are 16cm and 30cm, find its side.", a: "17 cm", opts: ["15", "17", "23", "None"] },
+            { q: "Two poles of heights 6m and 11m stand on ground. If distance between feet is 12m, find distance between tops.", a: "13 m", opts: ["5", "12", "13", "17"] },
+            { q: "In an isosceles right triangle, if hypotenuse is 5√2, find sides.", a: "5", opts: ["5", "10", "2.5", "None"] },
+            { q: "The foot of a 25m ladder is 7m from a wall. If the top slips 4m, how far will the foot slide?", a: "8 m (to 15m total)", opts: ["4", "8", "15", "None"] }
+        ],
+        "Tough": [
+            { q: "In ΔABC, if AD ⊥ BC and AD² = BD.DC, then ∠BAC is ____.", a: "90°", opts: ["45°", "60°", "90°", "120°"] },
+            { q: "In an equilateral triangle ABC, D is a point on BC such that BD = 1/3 BC. Then 9AD² = ____.", a: "7AB²", opts: ["7AB²", "8AB²", "9AB²", "None"] },
+            { q: "BL and CM are medians of ΔABC right angled at A. Then 4(BL² + CM²) = ____.", a: "5BC²", opts: ["3BC²", "4BC²", "5BC²", "None"] },
+            { q: "O is any point inside a rectangle ABCD. Then OA² + OC² = ____.", a: "OB² + OD²", opts: ["OB² + OD²", "AB² + BC²", "0", "None"] },
+            { q: "In ΔABC, ∠C=90°. If P, Q are points on sides CA, CB respectively, then AQ² + BP² = ____.", a: "AB² + PQ²", opts: ["AB² + PQ²", "AC² + BC²", "2AB²", "None"] },
+            { q: "The perpendicular from A on side BC of ΔABC intersects BC at D such that DB = 3CD. Then 2AB² = ____.", a: "2AC² + BC²", opts: ["2AC² + BC²", "AC² + BC²", "3AC²", "None"] },
+            { q: "A point O in the interior of ΔABC is joined to vertices. If OD ⊥ BC, OE ⊥ AC, OF ⊥ AB, then AF²+BD²+CE² = ____.", a: "AE²+CD²+BF²", opts: ["AE²+CD²+BF²", "0", "AB²+BC²+CA²", "None"] },
+            { q: "If the sides of a triangle are a, b, c and a² + b² = c², is the triangle right-angled?", a: "Yes", opts: ["Yes", "No", "Depends on c", "None"] },
+            { q: "Area of a right triangle is 30. If its altitude is 7m more than its base, find base.", a: "5 m", opts: ["5", "12", "7", "None"] },
+            { q: "Find the perimeter of a right triangle whose hypotenuse is 25cm and one side is 7cm.", a: "56 cm", opts: ["48", "56", "60", "None"] }
+        ]
+    }
+};
