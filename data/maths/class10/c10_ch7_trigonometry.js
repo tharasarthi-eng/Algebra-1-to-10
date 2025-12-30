2ca5e9c29f31e90cc64020db7bbb6229fe821824
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 21:04:07 +0530

    Update c10_ch7_trigonometry.js

--- data/maths/class10/c10_ch7_trigonometry.js
+++ data/maths/class10/c10_ch7_trigonometry.js
@@ -1,1 +1,117 @@
+window.currentChapterData = {
+    "Trigonometric Ratios": {
+        "Easy": [
+            { q: "In a right triangle, sin θ is the ratio of ____.", a: "Opposite/Hypotenuse", opts: ["Adjacent/Hypotenuse", "Opposite/Adjacent", "Opposite/Hypotenuse", "Hypotenuse/Opposite"] },
+            { q: "The reciprocal of sin θ is ____.", a: "cosec θ", opts: ["sec θ", "cosec θ", "tan θ", "cot θ"] },
+            { q: "If sin A = 3/5, then cosec A is ____.", a: "5/3", opts: ["4/5", "5/3", "3/4", "1"] },
+            { q: "In ΔABC right angled at B, tan A = ____.", a: "BC/AB", opts: ["AB/BC", "BC/AC", "BC/AB", "AB/AC"] },
+            { q: "The value of tan θ is the ratio of sin θ to ____.", a: "cos θ", opts: ["sec θ", "cos θ", "cot θ", "1"] },
+            { q: "If cos A = 4/5, find sin A.", a: "3/5", opts: ["3/5", "5/4", "4/3", "None"] },
+            { q: "Which ratio is defined as Adjacent/Hypotenuse?", a: "cos θ", opts: ["sin θ", "cos θ", "tan θ", "sec θ"] },
+            { q: "The maximum value of sin θ or cos θ can never exceed ____.", a: "1", opts: ["0", "1", "90", "Infinite"] },
+            { q: "In a right triangle, the longest side is always the ____.", a: "Hypotenuse", opts: ["Base", "Perpendicular", "Hypotenuse", "Median"] },
+            { q: "cot θ is the reciprocal of ____.", a: "tan θ", opts: ["sin θ", "cos θ", "tan θ", "sec θ"] }
+        ],
+        "Moderate": [
+            { q: "Given 15 cot A = 8, find sin A.", a: "15/17", opts: ["8/17", "15/17", "17/15", "17/8"] },
+            { q: "In ΔPQR, right-angled at Q, PQ = 3cm and PR = 6cm. Find ∠P.", a: "60°", opts: ["30°", "45°", "60°", "90°"] },
+            { q: "If sec θ = 13/12, calculate sin θ.", a: "5/13", opts: ["5/12", "12/13", "5/13", "1"] },
+            { q: "If ∠A and ∠B are acute angles such that cos A = cos B, then ____.", a: "∠A = ∠B", opts: ["∠A > ∠B", "∠A = ∠B", "∠A + ∠B = 90°", "None"] },
+            { q: "In ΔABC right angled at C, find the value of cos(A+B).", a: "0", opts: ["0", "1", "1/2", "√3/2"] },
+            { q: "If tan A = 4/3, find the value of (sin A + cos A).", a: "7/5", opts: ["1", "7/5", "5/7", "1/5"] },
+            { q: "The value of (sin² 30° + cos² 30°) is ____.", a: "1", opts: ["0", "1", "1/2", "2"] },
+            { q: "If 3 cot A = 4, check if (1 - tan²A)/(1 + tan²A) = cos²A - sin²A.", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "In a right triangle, if tan θ = 1, then the triangle is ____.", a: "Isosceles", opts: ["Equilateral", "Scalene", "Isosceles", "None"] },
+            { q: "Find sin A cos C + cos A sin C in ΔABC right angled at B, if tan A = 1/√3.", a: "1", opts: ["0", "1/2", "1", "√3/2"] }
+        ],
+        "Tough": [
+            { q: "If sin (A - B) = 1/2 and cos (A + B) = 1/2, find A and B.", a: "A=45°, B=15°", opts: ["A=30, B=30", "A=45, B=15", "A=60, B=0", "None"] },
+            { q: "If tan(A+B) = √3 and tan(A-B) = 1/√3, find A.", a: "45°", opts: ["30°", "45°", "60°", "90°"] },
+            { q: "If cosec θ = 2, cot θ = √3 p, find p.", a: "1", opts: ["1", "1/√3", "√3", "0"] },
+            { q: "In ΔOPQ, right-angled at P, OP = 7cm and OQ - PQ = 1cm. Find sin Q.", a: "7/25", opts: ["7/24", "24/25", "7/25", "None"] },
+            { q: "If sin θ + sin² θ = 1, then the value of (cos² θ + cos⁴ θ) is ____.", a: "1", opts: ["0", "1", "2", "1/2"] },
+            { q: "If cos A + cos² A = 1, find sin² A + sin⁴ A.", a: "1", opts: ["0", "1", "-1", "2"] },
+            { q: "If 5 tan θ = 4, then (5 sin θ - 3 cos θ) / (5 sin θ + 2 cos θ) = ____.", a: "1/6", opts: ["1/6", "5/6", "4/5", "0"] },
+            { q: "If x = r sin A cos C, y = r sin A sin C, z = r cos A, then x² + y² + z² = ____.", a: "r²", opts: ["r", "r²", "1", "0"] },
+            { q: "Find the value of (sec A + tan A)(1 - sin A).", a: "cos A", opts: ["sin A", "cos A", "sec A", "cosec A"] },
+            { q: "If a cos θ + b sin θ = m and a sin θ - b cos θ = n, then a² + b² = ____.", a: "m² + n²", opts: ["m² - n²", "m² + n²", "mn", "1"] }
+        ]
+    },
+    "Trigonometric Table & Values": {
+        "Easy": [
+            { q: "The value of sin 30° is ____.", a: "1/2", opts: ["0", "1/2", "√3/2", "1"] },
+            { q: "The value of tan 45° is ____.", a: "1", opts: ["0", "1", "1/√3", "√3"] },
+            { q: "Which of the following has a value of 0?", a: "sin 0°", opts: ["sin 0°", "cos 0°", "tan 45°", "sin 90°"] },
+            { q: "The value of cos 60° is equal to the value of ____.", a: "sin 30°", opts: ["sin 60°", "sin 30°", "tan 30°", "cos 30°"] },
+            { q: "What is the value of sec 0°?", a: "1", opts: ["0", "1", "Not defined", "2"] },
+            { q: "tan 90° is ____.", a: "Not defined", opts: ["0", "1", "Not defined", "Infinite"] },
+            { q: "Value of sin 90° + cos 90° is ____.", a: "1", opts: ["0", "1", "2", "None"] },
+            { q: "As θ increases from 0° to 90°, the value of sin θ ____.", a: "Increases", opts: ["Increases", "Decreases", "Stays same", "None"] },
+            { q: "As θ increases from 0° to 90°, the value of cos θ ____.", a: "Decreases", opts: ["Increases", "Decreases", "Stays same", "None"] },
+            { q: "Which value is √3?", a: "tan 60°", opts: ["sin 60°", "cos 30°", "tan 60°", "cot 60°"] }
+        ],
+        "Moderate": [
+            { q: "Evaluate: 2 tan² 45° + cos² 30° - sin² 60°.", a: "2", opts: ["1", "2", "3", "0"] },
+            { q: "Evaluate: (sin 30° + tan 45° - cosec 60°) / (sec 30° + cos 60° + cot 45°).", a: "(43 - 24√3)/11", opts: ["1", "0", "Complex", "None"] },
+            { q: "Find the value of (1 - tan² 45°) / (1 + tan² 45°).", a: "0", opts: ["tan 90°", "1", "sin 45°", "0"] },
+            { q: "Evaluate: cos 45° / (sec 30° + cosec 30°).", a: "(3√2 - √6)/8", opts: ["1/2", "(3√2 - √6)/8", "√3/2", "None"] },
+            { q: "sin 2A = 2 sin A is true when A is ____.", a: "0°", opts: ["0°", "30°", "45°", "60°"] },
+            { q: "Find the value of 2 tan 30° / (1 - tan² 30°).", a: "√3", opts: ["tan 60° (√3)", "sin 60°", "cos 60°", "None"] },
+            { q: "If sin θ = cos θ, then θ is ____.", a: "45°", opts: ["0°", "30°", "45°", "90°"] },
+            { q: "Evaluate: (5 cos² 60° + 4 sec² 30° - tan² 45°) / (sin² 30° + cos² 30°).", a: "67/12", opts: ["67/12", "55/12", "1", "0"] },
+            { q: "The value of tan 30° / cot 60° is ____.", a: "1", opts: ["1", "√3", "1/√3", "0"] },
+            { q: "Which is greater: sin 60° or cos 60°?", a: "sin 60°", opts: ["sin 60°", "cos 60°", "Equal", "None"] }
+        ],
+        "Tough": [
+            { q: "Evaluate: (sin 30° cos 45° + cos 30° sin 45°).", a: "(1+√3)/(2√2)", opts: ["1", "(1+√3)/(2√2)", "1/2", "None"] },
+            { q: "If A=30° and B=60°, verify sin(A+B) = sin A cos B + cos A sin B.", a: "True", opts: ["True", "False", "None", "Maybe"] },
+            { q: "Value of tan 1° tan 2° tan 3° ... tan 89° is ____.", a: "1", opts: ["0", "1", "89", "Not defined"] },
+            { q: "Find the value of (cos 0° + sin 45° + sin 30°)(sin 90° - cos 45° + cos 60°).", a: "7/4", opts: ["1", "7/4", "5/4", "None"] },
+            { q: "Evaluate: 4(sin⁴ 30° + cos⁴ 60°) - 3(cos² 45° - sin² 90°).", a: "2", opts: ["1", "2", "0", "None"] },
+            { q: "If θ = 30°, verify that sin 3θ = 3 sin θ - 4 sin³ θ.", a: "True", opts: ["True", "False", "None", "Maybe"] },
+            { q: "Value of cot 10° cot 20° cot 70° cot 80° is ____.", a: "1", opts: ["0", "1", "-1", "None"] },
+            { q: "If sin(A+B) = 1 and cos(A-B) = √3/2, find B.", a: "15°", opts: ["15°", "45°", "30°", "60°"] },
+            { q: "The value of (sin 45° + cos 45°)² is ____.", a: "2", opts: ["1", "2", "√2", "0"] },
+            { q: "If tan θ = a/b, find (cos θ + sin θ) / (cos θ - sin θ).", a: "(b+a)/(b-a)", opts: ["(a+b)/(a-b)", "(b+a)/(b-a)", "1", "None"] }
+        ]
+    },
+    "Trigonometric Identities": {
+        "Easy": [
+            { q: "Identity: sin² θ + cos² θ = ____.", a: "1", opts: ["0", "1", "tan² θ", "sec² θ"] },
+            { q: "Identity: 1 + tan² θ = ____.", a: "sec² θ", opts: ["cosec² θ", "sec² θ", "cot² θ", "1"] },
+            { q: "Identity: 1 + cot² θ = ____.", a: "cosec² θ", opts: ["cosec² θ", "sec² θ", "tan² θ", "None"] },
+            { q: "The value of (sec² θ - tan² θ) is ____.", a: "1", opts: ["0", "1", "-1", "None"] },
+            { q: "Find the value of 9 sec² A - 9 tan² A.", a: "9", opts: ["1", "9", "0", "-9"] },
+            { q: "The value of (1 - sin² θ) is ____.", a: "cos² θ", opts: ["sin² θ", "cos² θ", "tan² θ", "1"] },
+            { q: "Express cot A in terms of sin A.", a: "√(1 - sin²A) / sin A", opts: ["1/sin A", "sin A", "√(1 - sin²A) / sin A", "None"] },
+            { q: "Simplify: sin A cosec A.", a: "1", opts: ["0", "1", "sin² A", "None"] },
+            { q: "The value of (cosec² θ - cot² θ) is ____.", a: "1", opts: ["0", "1", "sec² θ", "None"] },
+            { q: "Is sin² θ = (sin θ)²?", a: "Yes", opts: ["Yes", "No", "Only for 45°", "None"] }
+        ],
+        "Moderate": [
+            { q: "Simplify: (1 + tan² A) / (1 + cot² A).", a: "tan² A", opts: ["sec² A", "tan² A", "cot² A", "-1"] },
+            { q: "The value of (1 + tan θ + sec θ)(1 + cot θ - cosec θ) is ____.", a: "2", opts: ["0", "1", "2", "-1"] },
+            { q: "Simplify: (sec A + tan A)(1 - sin A).", a: "cos A", opts: ["sin A", "cos A", "sec A", "1"] },
+            { q: "Prove: (sin A + cosec A)² + (cos A + sec A)² = 7 + tan² A + cot² A.", a: "True", opts: ["True", "False", "Only for A=45", "None"] },
+            { q: "If sin θ + cos θ = √2 cos θ, then cos θ - sin θ = ____.", a: "√2 sin θ", opts: ["√2 cos θ", "√2 sin θ", "2 sin θ", "None"] },
+            { q: "Find value of (1 + cot² A) sin² A.", a: "1", opts: ["0", "1", "sin² A", "cot² A"] },
+            { q: "If tan θ + cot θ = 5, find tan² θ + cot² θ.", a: "23", opts: ["25", "23", "27", "20"] },
+            { q: "Simplify: (cosec A - sin A)(sec A - cos A).", a: "1 / (tan A + cot A)", opts: ["1", "0", "1 / (tan A + cot A)", "None"] },
+            { q: "The value of sin⁴ A - cos⁴ A is ____.", a: "sin² A - cos² A", opts: ["1", "sin² A - cos² A", "0", "None"] },
+            { q: "If sec θ + tan θ = x, then sec θ - tan θ = ____.", a: "1/x", opts: ["x", "1/x", "-x", "0"] }
+        ],
+        "Tough": [
+            { q: "Prove: (tan A / (1 - cot A)) + (cot A / (1 - tan A)) = 1 + sec A cosec A.", a: "True", opts: ["True", "False", "None", "0"] },
+            { q: "If sin θ + cos θ = p and sec θ + cosec θ = q, then q(p² - 1) = ____.", a: "2p", opts: ["p", "2p", "p²", "0"] },
+            { q: "If tan θ + sin θ = m and tan θ - sin θ = n, then m² - n² = ____.", a: "4√mn", opts: ["√mn", "4√mn", "2√mn", "mn"] },
+            { q: "Prove: √[(1 + sin A) / (1 - sin A)] = sec A + tan A.", a: "True", opts: ["True", "False", "None", "Maybe"] },
+            { q: "If cosec θ - cot θ = q, find the value of cosec θ.", a: "(1 + q²) / 2q", opts: ["(1 - q²) / 2q", "(1 + q²) / 2q", "q", "None"] },
+            { q: "Simplify: (sin A - 2 sin³ A) / (2 cos³ A - cos A).", a: "tan A", opts: ["tan A", "cot A", "1", "0"] },
+            { q: "If x/a cos θ + y/b sin θ = 1 and x/a sin θ - y/b cos θ = 1, then x²/a² + y²/b² = ____.", a: "2", opts: ["1", "2", "0", "None"] },
+            { q: "The value of (1 + tan² A) / (1 + cot² A) is equal to ____.", a: "tan² A", opts: ["sec² A", "-1", "cot² A", "tan² A"] },
+            { q: "If cos θ + sin θ = √2 sin θ, find sin θ - cos θ.", a: "√2 cos θ", opts: ["√2 cos θ", "√2 sin θ", "1", "0"] },
+            { q: "Prove: (1 + sec A) / sec A = sin² A / (1 - cos A).", a: "True", opts: ["True", "False", "None", "0"] }
+        ]
+    }
+};
 
