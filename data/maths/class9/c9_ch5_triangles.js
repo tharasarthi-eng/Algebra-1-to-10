1a77037a2e9517e08e392770b76c7190f0a00170
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 20:46:05 +0530

    Update and rename c9_ch5_coord_geo.js to c9_ch5_triangles.js

---
+++ data/maths/class9/c9_ch5_triangles.js
@@ -0,0 +1,118 @@
+
+window.currentChapterData = {
+    "Parallel Lines & Transversals": {
+        "Easy": [
+            { q: "If two parallel lines are cut by a transversal, then each pair of corresponding angles are ____.", a: "Equal", opts: ["Equal", "Supplementary", "Complementary", "None"] },
+            { q: "The sum of interior angles on the same side of a transversal is ____.", a: "180°", opts: ["90°", "180°", "360°", "0°"] },
+            { q: "Alternate interior angles are equal only when the lines are ____.", a: "Parallel", opts: ["Intersecting", "Parallel", "Perpendicular", "Curved"] },
+            { q: "How many angles are formed when a transversal intersects two lines?", a: "8", opts: ["4", "6", "8", "10"] },
+            { q: "Two lines parallel to the same line are ____ to each other.", a: "Parallel", opts: ["Parallel", "Perpendicular", "Intersecting", "None"] },
+            { q: "In an 'F' shape, the angles formed are called ____ angles.", a: "Corresponding", opts: ["Alternate", "Corresponding", "Vertically Opposite", "Adjacent"] },
+            { q: "In a 'Z' shape, the angles formed are called ____ angles.", a: "Alternate Interior", opts: ["Alternate Interior", "Corresponding", "Co-interior", "Linear"] },
+            { q: "Angles which form a linear pair are always ____.", a: "Supplementary", opts: ["Equal", "Supplementary", "Complementary", "Acute"] },
+            { q: "If one angle of a linear pair is acute, the other must be ____.", a: "Obtuse", opts: ["Acute", "Obtuse", "Right", "Straight"] },
+            { q: "Vertically opposite angles are formed by two ____ lines.", a: "Intersecting", opts: ["Parallel", "Intersecting", "Perpendicular", "None"] }
+        ],
+        "Moderate": [
+            { q: "If a transversal intersects two parallel lines and one angle is 60°, find its co-interior angle.", a: "120°", opts: ["60°", "120°", "180°", "30°"] },
+            { q: "Find x if two parallel lines are cut by a transversal and interior angles on the same side are (2x+10) and (3x-20).", a: "38", opts: ["30", "38", "40", "45"] },
+            { q: "If the bisectors of a pair of alternate interior angles are parallel, then the two lines are ____.", a: "Parallel", opts: ["Parallel", "Perpendicular", "Intersecting", "None"] },
+            { q: "An angle is 20° less than its supplement. Find the angle.", a: "80°", opts: ["80°", "100°", "70°", "90°"] },
+            { q: "If L1 || L2 and L2 || L3, and angle x is on L1 and y is on L3 (corresponding), then x ____ y.", a: "=", opts: [">", "<", "=", "None"] },
+            { q: "The angle between the bisectors of two adjacent supplementary angles is ____.", a: "90°", opts: ["45°", "90°", "180°", "60°"] },
+            { q: "If two lines intersect such that one angle is 90°, then all other angles are ____.", a: "90°", opts: ["90°", "180°", "45°", "Variable"] },
+            { q: "In a triangle, the exterior angle is equal to the sum of ____.", a: "Two interior opposite angles", opts: ["All interior angles", "Two interior opposite angles", "Adjacent angles", "None"] },
+            { q: "If three lines meet at a point, how many pairs of vertically opposite angles are formed?", a: "6", opts: ["3", "4", "6", "12"] },
+            { q: "Find the angle which is four times its complement.", a: "72°", opts: ["18°", "72°", "36°", "144°"] }
+        ],
+        "Tough": [
+            { q: "If a transversal intersects two lines such that the bisectors of a pair of corresponding angles are parallel, then the lines are ____.", a: "Parallel", opts: ["Parallel", "Perpendicular", "Not necessarily parallel", "None"] },
+            { q: "In a figure, if AB || CD, CD || EF and y:z = 3:7, find x (where x,y are co-interior and x,z are alternate).", a: "126°", opts: ["54°", "126°", "100°", "70°"] },
+            { q: "Two parallel lines are cut by a transversal. The bisectors of the interior angles on the same side intersect at ____.", a: "90°", opts: ["45°", "60°", "90°", "180°"] },
+            { q: "If the ratio of two co-interior angles is 4:5, find the larger angle.", a: "100°", opts: ["80°", "100°", "90°", "110°"] },
+            { q: "The sides AB and AC of triangle ABC are produced to P and Q. If bisectors of ∠PBC and ∠QCB meet at O, then ∠BOC is ____.", a: "90° - 1/2 ∠A", opts: ["90° + 1/2 ∠A", "90° - 1/2 ∠A", "180° - ∠A", "1/2 ∠A"] },
+            { q: "Prove that the sum of angles of a triangle is 180° using ____.", a: "Parallel line axiom", opts: ["SAS rule", "Parallel line axiom", "Linear pair", "None"] },
+            { q: "If a line is perpendicular to one of two parallel lines, it is ____ to the other.", a: "Perpendicular", opts: ["Parallel", "Perpendicular", "Inclined", "None"] },
+            { q: "Number of transversals that can be drawn for two parallel lines is ____.", a: "Infinite", opts: ["1", "2", "4", "Infinite"] },
+            { q: "In triangle ABC, if ∠B = ∠C = 45°, then the triangle is ____.", a: "Right Isosceles", opts: ["Equilateral", "Right Isosceles", "Obtuse", "Scalene"] },
+            { q: "If an angle is 1/2 of its supplement, find its complement.", a: "30°", opts: ["60°", "30°", "120°", "0°"] }
+        ]
+    },
+    "Congruence of Triangles": {
+        "Easy": [
+            { q: "Two figures are congruent if they have the same shape and ____.", a: "Size", opts: ["Color", "Size", "Area", "Angle"] },
+            { q: "The symbol for congruence is ____.", a: "≅", opts: ["=", "≅", "||", "~"] },
+            { q: "If ΔABC ≅ ΔPQR, then side AB corresponds to ____.", a: "PQ", opts: ["PQ", "QR", "PR", "BC"] },
+            { q: "SAS congruence stands for Side-____-Side.", a: "Angle", opts: ["Area", "Altitude", "Angle", "Apex"] },
+            { q: "In a triangle, angles opposite to equal sides are ____.", a: "Equal", opts: ["Equal", "Supplementary", "Unequal", "None"] },
+            { q: "Which of these is NOT a criteria for congruence?", a: "AAA", opts: ["SSS", "SAS", "ASA", "AAA"] },
+            { q: "An equilateral triangle has all angles equal to ____.", a: "60°", opts: ["45°", "60°", "90°", "180°"] },
+            { q: "If all three sides of one triangle are equal to three sides of another, they are congruent by ____.", a: "SSS", opts: ["SSS", "SAS", "ASA", "RHS"] },
+            { q: "RHS congruence applies to ____ triangles.", a: "Right-angled", opts: ["Equilateral", "Isosceles", "Right-angled", "Scalene"] },
+            { q: "CPCT stands for Corresponding Parts of ____ Triangles.", a: "Congruent", opts: ["Common", "Congruent", "Calculated", "Circular"] }
+        ],
+        "Moderate": [
+            { q: "In ΔABC, if AB=AC and ∠A=50°, find ∠B.", a: "65°", opts: ["50°", "65°", "80°", "130°"] },
+            { q: "If the vertical angle of an isosceles triangle is 100°, find the base angles.", a: "40°, 40°", opts: ["50°, 50°", "40°, 40°", "80°, 80°", "None"] },
+            { q: "In ΔABC and ΔPQR, AB=PQ, BC=QR, and ∠B=∠Q. Name the congruence rule.", a: "SAS", opts: ["SSS", "SAS", "ASA", "RHS"] },
+            { q: "If ΔABC ≅ ΔLKM, then side AC is equal to ____.", a: "LM", opts: ["LK", "KM", "LM", "None"] },
+            { q: "In an isosceles triangle, the altitude from the vertex ____ the base.", a: "Bisects", opts: ["Is twice", "Bisects", "Is half", "None"] },
+            { q: "If two angles and the included side of one triangle are equal to those of another, they are congruent by ____.", a: "ASA", opts: ["SSS", "SAS", "ASA", "AAS"] },
+            { q: "Can two triangles be congruent if they have the same area?", a: "Not necessarily", opts: ["Yes", "No", "Not necessarily", "Only if squares"] },
+            { q: "In ΔABC, if ∠A=∠B=∠C, the triangle is ____.", a: "Equilateral", opts: ["Isosceles", "Equilateral", "Right-angled", "Scalene"] },
+            { q: "If the hypotenuse and one side of a right triangle are equal to those of another, they are congruent by ____.", a: "RHS", opts: ["SSS", "SAS", "ASA", "RHS"] },
+            { q: "In ΔABC, if AB=AC and ∠B=70°, find ∠A.", a: "40°", opts: ["70°", "40°", "110°", "180°"] }
+        ],
+        "Tough": [
+            { q: "If ΔABC ≅ ΔPQR and ΔABC is not congruent to ΔRPQ, then which is true?", a: "BC = QR", opts: ["AB = PQ", "BC = QR", "Both A & B", "QR = PR"] },
+            { q: "In ΔABC, the bisector of ∠A is perpendicular to side BC. The triangle is ____.", a: "Isosceles", opts: ["Scalene", "Isosceles", "Equilateral", "None"] },
+            { q: "Two sides of a triangle are 5cm and 1.5cm. The third side cannot be ____.", a: "3.4cm", opts: ["3.6cm", "4.1cm", "3.4cm", "4.8cm"] },
+            { q: "In ΔPQR, if ∠R > ∠Q, then ____.", a: "PQ > PR", opts: ["QR > PR", "PQ > PR", "PQ < PR", "QR < PQ"] },
+            { q: "In a right triangle, the side opposite to the right angle is the ____ and is the longest.", a: "Hypotenuse", opts: ["Base", "Altitude", "Hypotenuse", "Median"] },
+            { q: "If S is any point on the base BC of an isosceles ΔABC, then ____.", a: "AB > AS", opts: ["AB < AS", "AB = AS", "AB > AS", "None"] },
+            { q: "The perimeter of a triangle is ____ the sum of its three medians.", a: "Greater than", opts: ["Greater than", "Less than", "Equal to", "Half"] },
+            { q: "If the diagonals of a quadrilateral bisect each other at right angles, it is a ____.", a: "Rhombus", opts: ["Parallelogram", "Rectangle", "Rhombus", "Trapezium"] },
+            { q: "In ΔABC, if AD is the median, then AB + AC is ____ 2AD.", a: ">", opts: [">", "<", "=", "None"] },
+            { q: "A point equidistant from two intersecting lines lies on the ____.", a: "Angle bisector", opts: ["Perpendicular bisector", "Angle bisector", "Median", "None"] }
+        ]
+    },
+    "Inequalities in a Triangle": {
+        "Easy": [
+            { q: "In a triangle, the angle opposite to the longer side is ____.", a: "Larger", opts: ["Smaller", "Larger", "Equal", "90°"] },
+            { q: "In a triangle, the side opposite to the larger angle is ____.", a: "Longer", opts: ["Shorter", "Longer", "Equal", "None"] },
+            { q: "The sum of any two sides of a triangle is ____ than the third side.", a: "Greater", opts: ["Greater", "Less", "Equal", "None"] },
+            { q: "The difference between any two sides of a triangle is ____ than the third side.", a: "Less", opts: ["Greater", "Less", "Equal", "None"] },
+            { q: "Can a triangle have sides 2cm, 3cm, and 5cm?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "In a right-angled triangle, which side is the longest?", a: "Hypotenuse", opts: ["Base", "Perpendicular", "Hypotenuse", "None"] },
+            { q: "If sides of a triangle are 6, 7, 8, which angle is largest?", a: "Opposite to 8", opts: ["Opposite to 6", "Opposite to 7", "Opposite to 8", "All equal"] },
+            { q: "Is it possible to form a triangle with angles 40°, 40°, 100°?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "The sum of angles of a triangle is ____.", a: "180°", opts: ["90°", "180°", "360°", "270°"] },
+            { q: "If two sides of a triangle are equal, the triangle is ____.", a: "Isosceles", opts: ["Scalene", "Isosceles", "Equilateral", "Right"] }
+        ],
+        "Moderate": [
+            { q: "In ΔABC, if ∠A=40° and ∠B=60°, which is the longest side?", a: "AB", opts: ["BC", "AC", "AB", "None"] },
+            { q: "If the sides of a triangle are (x-1), x, (x+1), find the range of x.", a: "x > 2", opts: ["x > 1", "x > 2", "x < 5", "None"] },
+            { q: "In ΔABC, if AB=5cm, BC=7cm, then AC must be between ____.", a: "2 and 12", opts: ["5 and 7", "2 and 12", "0 and 12", "None"] },
+            { q: "Which of the following can be the sides of a triangle?", a: "10, 12, 14", opts: ["2, 4, 6", "1, 2, 4", "10, 12, 14", "5, 5, 11"] },
+            { q: "If O is a point in the interior of ΔABC, then OA+OB+OC is ____ 1/2(AB+BC+CA).", a: ">", opts: [">", "<", "=", "None"] },
+            { q: "In ΔPQR, if PQ=QR, then which angle is the smallest?", a: "Depends on PR", opts: ["∠P", "∠Q", "∠R", "Depends on PR"] },
+            { q: "The sum of altitudes of a triangle is ____ than its perimeter.", a: "Less", opts: ["Less", "Greater", "Equal", "None"] },
+            { q: "In a triangle, the side opposite the smallest angle is the ____ side.", a: "Shortest", opts: ["Shortest", "Longest", "Median", "None"] },
+            { q: "If x, y, z are sides of a triangle, then x+y-z is always ____.", a: "Positive", opts: ["Positive", "Negative", "Zero", "None"] },
+            { q: "Can a triangle have sides √2, √3, and √5?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] }
+        ],
+        "Tough": [
+            { q: "In ΔABC, if ∠A > ∠B and ∠B > ∠C, then which is true?", a: "BC > AC > AB", opts: ["AB > AC > BC", "BC > AC > AB", "AC > BC > AB", "None"] },
+            { q: "If P is a point in the interior of ΔABC, then (PA + PB) is ____ (AC + BC).", a: "<", opts: [">", "<", "=", "None"] },
+            { q: "The sum of the lengths of the three medians of a triangle is ____ the perimeter.", a: "Less than", opts: ["Greater than", "Less than", "Equal to", "Double"] },
+            { q: "If the perimeter of a triangle is 12cm and sides are integers, how many such triangles exist?", a: "3", opts: ["1", "2", "3", "4"] },
+            { q: "In ΔABC, AD is the median to BC. Is AB + AC > 2AD?", a: "Yes", opts: ["Yes", "No", "Equal", "None"] },
+            { q: "The smallest side of a triangle is 4cm and the second side is 7cm. What is the maximum possible integer length of the third side?", a: "10cm", opts: ["10cm", "11cm", "7cm", "12cm"] },
+            { q: "If a, b, c are sides of a triangle, then (a+b+c)² is always ____ 4(ab+bc+ca).", a: "<", opts: [">", "<", "=", "None"] },
+            { q: "In a quadrilateral ABCD, is AB+BC+CD+DA > AC+BD?", a: "Yes", opts: ["Yes", "No", "Equal", "None"] },
+            { q: "If a, b, c are sides of a triangle, then a² + b² + c² is ____ 2(ab+bc+ca).", a: "<", opts: [">", "<", "=", "None"] },
+            { q: "In ΔABC, if ∠A = 100°, then side BC is the ____ side.", a: "Longest", opts: ["Longest", "Shortest", "Median", "None"] }
+        ]
+    }
+};
+             
