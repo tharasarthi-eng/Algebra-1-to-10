f902933955d182d4f6d1e90ca20d4ab5974d079b
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 20:15:29 +0530

    Update and rename c8_ch5_algebraic_expr.js to c8_ch5_algebra.js

---
+++ data/maths/class8/c8_ch5_algebra.js
@@ -0,0 +1,193 @@
+window.currentChapterData = {
+    "Terms, Factors & Coefficients": {
+        "Easy": [
+            { q: "Expressions are made up of ____.", a: "Terms", opts: ["Terms", "Factors", "Equations", "Numbers"] },
+            { q: "How many terms are in the expression 2x - 3y + 5?", a: "3", opts: ["1", "2", "3", "4"] },
+            { q: "In the term -7xy, what is the numerical coefficient?", a: "-7", opts: ["7", "-7", "x", "xy"] },
+            { q: "What are the factors of 5xy?", a: "5, x, y", opts: ["5, x", "x, y", "5, x, y", "5x, y"] },
+            { q: "An expression containing only one term is called a ____.", a: "Monomial", opts: ["Monomial", "Binomial", "Trinomial", "Polynomial"] },
+            { q: "An expression with two terms is a ____.", a: "Binomial", opts: ["Monomial", "Binomial", "Trinomial", "None"] },
+            { q: "Is 4x + 7 a binomial?", a: "Yes", opts: ["Yes", "No", "Only if x=1", "None"] },
+            { q: "Identify the coefficient of x² in 3x² - 5x + 2.", a: "3", opts: ["3", "-5", "2", "x"] },
+            { q: "Like terms have the same ____ factors.", a: "Algebraic", opts: ["Numerical", "Algebraic", "Constant", "None"] },
+            { q: "Which of these is a trinomial?", a: "a + b + c", opts: ["ab", "a + b", "a + b + c", "abc"] }
+        ],
+        "Moderate": [
+            { q: "Identify like terms: 7x, 14y, -13x, 5xy.", a: "7x and -13x", opts: ["7x and 14y", "7x and -13x", "14y and 5xy", "None"] },
+            { q: "What is the numerical coefficient of -x?", a: "-1", opts: ["1", "-1", "0", "x"] },
+            { q: "Classification of x² - y² is ____.", a: "Binomial", opts: ["Monomial", "Binomial", "Trinomial", "Constant"] },
+            { q: "Terms are added to form ____.", a: "Expressions", opts: ["Factors", "Expressions", "Coefficients", "Variables"] },
+            { q: "In the expression 4x²y - 3xy², are the terms 'like'?", a: "No", opts: ["Yes", "No", "Depends on x,y", "None"] },
+            { q: "Find the coefficient of y in the expression 2x - y + 5.", a: "-1", opts: ["1", "-1", "y", "5"] },
+            { q: "The factors of the term -4x² are ____.", a: "-4, x, x", opts: ["-4, x", "4, x, x", "-4, x, x", "-4x, x"] },
+            { q: "Which of the following is a polynomial?", a: "All of these", opts: ["Monomial", "Binomial", "Trinomial", "All of these"] },
+            { q: "Expression for 'Sum of x and y subtracted from their product' is?", a: "xy - (x+y)", opts: ["(x+y) - xy", "xy - x+y", "xy - (x+y)", "x+y+xy"] },
+            { q: "Coefficient of a²b in -a²b is ____.", a: "-1", opts: ["1", "-1", "0", "a"] }
+        ],
+        "Tough": [
+            { q: "Find the sum of numerical coefficients of all terms in: x² - 5xy + 6y² - 1.", a: "1", opts: ["1", "11", "12", "0"] },
+            { q: "A polynomial can have ____ number of terms.", a: "Any finite", opts: ["Exactly 3", "Up to 5", "Any finite", "Infinite"] },
+            { q: "If the algebraic factors of two terms are the same, they are ____.", a: "Like terms", opts: ["Equal terms", "Like terms", "Identical", "None"] },
+            { q: "Write the factors of 1.2ab - 2.4b + 3.6a as a tree.", a: "Tree structure", opts: ["1.2, a, b", "2.4, b", "3.6, a", "All of these"] },
+            { q: "Which is NOT a polynomial?", a: "x + 1/x", opts: ["x² + 2x", "5", "x + 1/x", "0"] },
+            { q: "In the term -2/3 x²y, the constant part is ____.", a: "-2/3", opts: ["2/3", "-2/3", "x²y", "None"] },
+            { q: "Identify the coefficient of 'x' in the expression (x+a)(x+b).", a: "a+b", opts: ["1", "ab", "a+b", "x"] },
+            { q: "How many terms are in a product of a monomial and a trinomial?", a: "3", opts: ["1", "3", "4", "6"] },
+            { q: "Degree of the polynomial 4x² - 3x + 5 is ____.", a: "2", opts: ["1", "2", "3", "0"] },
+            { q: "Coefficient of x in -3ax is ____.", a: "-3a", opts: ["-3", "a", "-3a", "x"] }
+        ]
+    },
+    "Addition & Subtraction of Expressions": {
+        "Easy": [
+            { q: "Add: 7x and 3x.", a: "10x", opts: ["10", "10x", "21x", "4x"] },
+            { q: "Subtract 4a from 7a.", a: "3a", opts: ["3", "11a", "3a", "-3a"] },
+            { q: "Sum of xy, -xy, 2xy is ____.", a: "2xy", opts: ["0", "xy", "2xy", "4xy"] },
+            { q: "Simplify: a + b + a - b.", a: "2a", opts: ["2a", "2b", "0", "ab"] },
+            { q: "Add: (x + y) and (x - y).", a: "2x", opts: ["2x", "2y", "x+y", "0"] },
+            { q: "Subtract x from -x.", a: "-2x", opts: ["0", "2x", "-2x", "x²"] },
+            { q: "Simplify: 3x² + 2x².", a: "5x²", opts: ["5x", "5x²", "6x²", "x²"] },
+            { q: "Sum of 2, 3x, and 4 is ____.", a: "3x + 6", opts: ["9x", "3x+6", "7x", "None"] },
+            { q: "Subtract 5xy from 10xy.", a: "5xy", opts: ["5", "15xy", "5xy", "0"] },
+            { q: "Add: 4p²q and -3p²q.", a: "p²q", opts: ["p²q", "7p²q", "-p²q", "0"] }
+        ],
+        "Moderate": [
+            { q: "Add: 7xy + 5yz - 3zx, 4yz + 9zx - 4y, -3xz + 5x - 2xy.", a: "5xy + 9yz + 3zx + 5x - 4y", opts: ["5xy + 9yz + 3zx + 5x - 4y", "9xy + 9yz + 3zx", "None", "0"] },
+            { q: "Subtract 4a - 7ab + 3b + 12 from 12a - 9ab + 5b - 3.", a: "8a - 2ab + 2b - 15", opts: ["8a - 16ab + 8b + 9", "8a - 2ab + 2b - 15", "16a - 2ab + 8b", "None"] },
+            { q: "What should be added to x² + xy + y² to get 2x² + 3xy?", a: "x² + 2xy - y²", opts: ["x² + 2xy + y²", "x² + 2xy - y²", "3x² + 4xy", "None"] },
+            { q: "What should be subtracted from 2a + 8b + 10 to get -3a + 7b + 16?", a: "5a + b - 6", opts: ["a + 15b + 26", "5a + b - 6", "-a - b + 6", "None"] },
+            { q: "Simplify: (a² - b²) - (b² - a²).", a: "2a² - 2b²", opts: ["0", "2a² - 2b²", "a² + b²", "None"] },
+            { q: "Sum of ab - bc, bc - ca, and ca - ab is ____.", a: "0", opts: ["abc", "a+b+c", "0", "2ab"] },
+            { q: "Find the perimeter of a triangle with sides x+y, y+z, z+x.", a: "2(x+y+z)", opts: ["x+y+z", "2x+2y+2z", "2(x+y+z)", "Both B & C"] },
+            { q: "Subtract x² - y² from y² - x².", a: "2y² - 2x²", opts: ["0", "2y² - 2x²", "2x² - 2y²", "None"] },
+            { q: "Simplify: x - [y - {x - (y - x)}].", a: "x - 2y", opts: ["x", "y", "x - 2y", "3x - 2y"] },
+            { q: "Add: 3m - 4n, 2n - 3m, m + n.", a: "m - n", opts: ["m+n", "m-n", "0", "6m"] }
+        ],
+        "Tough": [
+            { q: "From the sum of 3x - y + 11 and -y - 11, subtract 3x - y - 11.", a: "-y + 11", opts: ["3x - y", "-y + 11", "3x + y", "0"] },
+            { q: "Simplify: (x² + y² + 2xy) + (x² + y² - 2xy) - 2(x² + y²).", a: "0", opts: ["4xy", "2x² + 2y²", "0", "None"] },
+            { q: "If A = x + y, B = x - y, C = 2x - 3, find A + B - C.", a: "3", opts: ["4x - 3", "2x + 3", "3", "0"] },
+            { q: "Subtract the sum of 2x - x² + 5 and -4x - 3x² + 7 from 5.", a: "4x² + 2x - 7", opts: ["4x² + 2x - 7", "-4x² - 2x + 7", "x² + x", "None"] },
+            { q: "What should be subtracted from 1 to get x² - xy + y²?", a: "1 - x² + xy - y²", opts: ["x² - xy + y² - 1", "1 - x² + xy - y²", "x² + y²", "None"] },
+            { q: "Simplify: p - (p - q) - q - (q - p).", a: "p - q", opts: ["0", "p+q", "p-q", "2p-2q"] },
+            { q: "Find distance between (x+5) and (x-2) on number line.", a: "7", opts: ["3", "7", "2x+3", "None"] },
+            { q: "Sum of two expressions is x². If one is x²/2 - xy, find other.", a: "x²/2 + xy", opts: ["x²/2 + xy", "x² - xy", "xy", "None"] },
+            { q: "Add: 1/2 x, 1/3 x, 1/4 x.", a: "13/12 x", opts: ["1/9 x", "13/12 x", "1x", "None"] },
+            { q: "Is addition of algebraic expressions associative?", a: "Yes", opts: ["Yes", "No", "Only for like terms", "None"] }
+        ]
+    },
+    "Multiplication of Expressions": {
+        "Easy": [
+            { q: "Product of 4x and 3 is ____.", a: "12x", opts: ["7x", "12x", "12", "x"] },
+            { q: "Multiply: x * x.", a: "x²", opts: ["2x", "x²", "x", "0"] },
+            { q: "Product of 5x and 2y is ____.", a: "10xy", opts: ["7xy", "10x", "10y", "10xy"] },
+            { q: "4 * 3xy = ?", a: "12xy", opts: ["12x", "12y", "12xy", "7xy"] },
+            { q: "Find: x² * x³.", a: "x⁵", opts: ["x⁶", "x⁵", "2x⁵", "x"] },
+            { q: "Multiply monomial 3x with binomial (y + 2).", a: "3xy + 6x", opts: ["3xy + 2", "3xy + 6x", "3x + y + 2", "None"] },
+            { q: "Area of rectangle with length 4x and breadth 3y?", a: "12xy", opts: ["7xy", "12x", "12y", "12xy"] },
+            { q: "(-4p) * (7q) = ?", a: "-28pq", opts: ["28pq", "-28pq", "3pq", "-3pq"] },
+            { q: "Product of 0 and any expression is ____.", a: "0", opts: ["1", "0", "The expression", "None"] },
+            { q: "Multiply: 2a, 3b, 4c.", a: "24abc", opts: ["9abc", "24abc", "12abc", "None"] }
+        ],
+        "Moderate": [
+            { q: "Multiply: (x + 2)(x + 3).", a: "x² + 5x + 6", opts: ["x² + 6", "x² + 5x + 6", "2x + 5", "x² + 6x + 5"] },
+            { q: "Find product: (2a + 3b)(4a - 5b).", a: "8a² + 2ab - 15b²", opts: ["8a² - 15b²", "8a² + 2ab - 15b²", "6a - 2b", "None"] },
+            { q: "Simplify: a(a² + 1) + 5.", a: "a³ + a + 5", opts: ["a³ + 5", "a³ + a + 5", "3a + 5", "None"] },
+            { q: "Evaluate: 2x(3x + 5y) for x=1, y=2.", a: "26", opts: ["16", "26", "20", "30"] },
+            { q: "Find area of a square with side 3x.", a: "9x²", opts: ["6x", "9x", "9x²", "3x²"] },
+            { q: "Multiply: (3/4 a²) by (4/3 b²).", a: "a²b²", opts: ["ab", "a²b²", "12/12", "None"] },
+            { q: "Simplify: x(x-y) + y(y-x).", a: "x² - 2xy + y²", opts: ["x² + y²", "0", "x² - 2xy + y²", "None"] },
+            { q: "Product of (a+b) and (a-b) is ____.", a: "a² - b²", opts: ["a² + b²", "a² - b²", "2a", "0"] },
+            { q: "Volume of a cuboid with dimensions 2ax, 3by, 5cz?", a: "30abcxyz", opts: ["10abc", "30abcxyz", "30", "None"] },
+            { q: "Multiply: (x - 4)(x - 4).", a: "x² - 8x + 16", opts: ["x² - 16", "x² + 16", "x² - 8x + 16", "None"] }
+        ],
+        "Tough": [
+            { q: "Simplify: (x + y)(2x + y) + (x + 2y)(x - y).", a: "3x² + 4xy - y²", opts: ["3x² + 4xy - y²", "2x² + y²", "3x² + xy", "None"] },
+            { q: "Multiply: (a + b + c)(a + b - c).", a: "(a+b)² - c²", opts: ["a² + b² - c²", "(a+b)² - c²", "a+b+c", "None"] },
+            { q: "Find value of (x - y)(x + y)(x² + y²).", a: "x⁴ - y⁴", opts: ["x² - y²", "x⁴ + y⁴", "x⁴ - y⁴", "0"] },
+            { q: "Simplify: 1.5x(0.5x + 2y) - 0.75x².", a: "3xy", opts: ["0", "3xy", "1.5x² + 3xy", "None"] },
+            { q: "If (x + 1/x) = 3, find x² + 1/x².", a: "7", opts: ["9", "7", "11", "6"] },
+            { q: "Multiply: (2x² - 5x + 4)(x - 3).", a: "2x³ - 11x² + 19x - 12", opts: ["2x³ - 12", "2x³ - 11x² + 19x - 12", "2x² - 6x", "None"] },
+            { q: "Evaluate: (99)² using (100 - 1)².", a: "9801", opts: ["9900", "9801", "9899", "10001"] },
+            { q: "Simplify: (a + b)(c - d) + (a - b)(c + d) + 2(ac + bd).", a: "4ac", opts: ["0", "4ac", "2ac + 2bd", "None"] },
+            { q: "Find the coefficient of x² in (x + 3)(x + 4)(x + 5).", a: "12", opts: ["1", "12", "47", "60"] },
+            { q: "Product of (x/2 - y/3) and (x/2 + y/3).", a: "x²/4 - y²/9", opts: ["x/4 - y/9", "x²/4 - y²/9", "x²/2 - y²/3", "None"] }
+        ]
+    },
+    "Algebraic Identities": {
+        "Easy": [
+            { q: "Identity: (a + b)² = ____.", a: "a² + 2ab + b²", opts: ["a² + b²", "a² + 2ab + b²", "a² - 2ab + b²", "2a + 2b"] },
+            { q: "Identity: (a - b)² = ____.", a: "a² - 2ab + b²", opts: ["a² + b²", "a² - b²", "a² - 2ab + b²", "None"] },
+            { q: "Identity: (a + b)(a - b) = ____.", a: "a² - b²", opts: ["a² + b²", "a² - b²", "(a-b)²", "0"] },
+            { q: "Use identity: (x + 3)² = ?", a: "x² + 6x + 9", opts: ["x² + 9", "x² + 6x + 9", "x² + 3x + 9", "2x + 6"] },
+            { q: "Use identity: (2x - 1)² = ?", a: "4x² - 4x + 1", opts: ["4x² - 1", "4x² + 1", "4x² - 4x + 1", "None"] },
+            { q: "Value of (10 + 1)² is ____.", a: "121", opts: ["101", "111", "121", "110"] },
+            { q: "Which identity is (x+a)(x+b) = x² + (a+b)x + ab?", a: "Identity IV", opts: ["Identity I", "Identity II", "Identity III", "Identity IV"] },
+            { q: "Evaluate 51² - 49².", a: "200", opts: ["2", "100", "200", "400"] },
+            { q: "Is (a-b)² same as (b-a)²?", a: "Yes", opts: ["Yes", "No", "Only if a=b", "None"] },
+            { q: "Find (100 - 1)(100 + 1).", a: "9999", opts: ["10001", "9999", "10000", "9900"] }
+        ],
+        "Moderate": [
+            { q: "Evaluate 103 x 104 using (x+a)(x+b).", a: "10712", opts: ["10712", "10700", "10812", "None"] },
+            { q: "Find (4x - 5y)². ", a: "16x² - 40xy + 25y²", opts: ["16x² - 25y²", "16x² + 25y²", "16x² - 40xy + 25y²", "None"] },
+            { q: "Simplify: (7m - 8n)² + (7m + 8n)².", a: "98m² + 128n²", opts: ["196m²", "256n²", "98m² + 128n²", "0"] },
+            { q: "Evaluate 10.5 x 9.5 using (a+b)(a-b).", a: "99.75", opts: ["100", "99.75", "99.25", "100.25"] },
+            { q: "If x² + 1/x² = 27, find x - 1/x.", a: "5", opts: ["5", "√27", "25", "29"] },
+            { q: "Find the square of 998 using identity.", a: "996004", opts: ["996004", "998004", "996000", "None"] },
+            { q: "Expand (2x/3 + 3/2)². ", a: "4x²/9 + 2x + 9/4", opts: ["4x²/9 + 9/4", "4x²/9 + 2x + 9/4", "x² + 2x + 1", "None"] },
+            { q: "Evaluate 153² - 147².", a: "1800", opts: ["6", "300", "1800", "3600"] },
+            { q: "Find value of (a² - b²) if a=10.1, b=9.9.", a: "4", opts: ["0.2", "4", "40", "0.4"] },
+            { q: "Simplify: (1.1m - 0.4)(1.1m + 0.4).", a: "1.21m² - 0.16", opts: ["1.21m² + 0.16", "1.21m² - 0.16", "2.2m - 0.8", "None"] }
+        ],
+        "Tough": [
+            { q: "If x + 1/x = 5, find x⁴ + 1/x⁴.", a: "527", opts: ["625", "527", "529", "525"] },
+            { q: "Evaluate: (0.98)².", a: "0.9604", opts: ["0.9604", "0.96", "0.9804", "None"] },
+            { q: "Simplify: (a + b + c)² - (a - b - c)².", a: "4a(b + c)", opts: ["0", "4ab + 4ac", "4a(b + c)", "Both B & C"] },
+            { q: "Find x if 4x = 52² - 48².", a: "100", opts: ["100", "400", "50", "25"] },
+            { q: "If x - y = 7 and xy = 9, find x² + y².", a: "67", opts: ["49", "67", "58", "40"] },
+            { q: "Evaluate 105 x 95 using identity.", a: "9975", opts: ["9975", "10000", "9925", "None"] },
+            { q: "Simplify: (x - y)(x + y)(x² + y²)(x⁴ + y⁴).", a: "x⁸ - y⁸", opts: ["x⁴ - y⁴", "x⁸ - y⁸", "x¹⁶ - y¹⁶", "0"] },
+            { q: "If a + b = 10 and a² + b² = 58, find ab.", a: "21", opts: ["21", "42", "11", "None"] },
+            { q: "Evaluate (1.02)² - (0.98)².", a: "0.08", opts: ["0.08", "0.04", "0.8", "0.008"] },
+            { q: "Find the value of (3x + 2y)² - (3x - 2y)².", a: "24xy", opts: ["0", "12xy", "24xy", "18x² + 8y²"] }
+        ]
+    },
+    "Applied Algebra (Word Problems)": {
+        "Easy": [
+            { q: "Area of a square with side (x+2) is ____.", a: "x² + 4x + 4", opts: ["x² + 4", "x² + 4x + 4", "4x + 8", "None"] },
+            { q: "A rectangle has length 5x and breadth 3x. Its area is?", a: "15x²", opts: ["8x", "15x", "15x²", "2x"] },
+            { q: "If cost of one pen is (x+5), cost of x pens is?", a: "x² + 5x", opts: ["x+5", "2x+5", "x² + 5x", "5x"] },
+            { q: "Perimeter of square with side 2a?", a: "8a", opts: ["4a", "8a", "4a²", "None"] },
+            { q: "Volume of cube with side m?", a: "m³", opts: ["3m", "m²", "m³", "6m²"] },
+            { q: "Expression for '5 times x added to 7 times y'.", a: "5x + 7y", opts: ["12xy", "5x+7y", "35xy", "None"] },
+            { q: "Distance = Speed x Time. If Speed = x and Time = (x+1), Distance = ?", a: "x² + x", opts: ["2x+1", "x² + x", "x/x+1", "None"] },
+            { q: "If a = 2, find value of a² + 2a + 1.", a: "9", opts: ["5", "7", "9", "1"] },
+            { q: "Price of a notebook is ₹x and pen is ₹y. Total cost of 2 notebooks and 3 pens?", a: "2x + 3y", opts: ["5xy", "2x+3y", "6xy", "None"] },
+            { q: "Find area of rectangle with dimensions 2a and 3a.", a: "6a²", opts: ["5a", "6a", "6a²", "None"] }
+        ],
+        "Moderate": [
+            { q: "Area of a triangle with base 4x and height 3x is?", a: "6x²", opts: ["12x²", "7x", "6x²", "None"] },
+            { q: "The base of a parallelogram is (x+4) and height is x. Area?", a: "x² + 4x", opts: ["2x+4", "x²+4x", "4x", "None"] },
+            { q: "Find the volume of a rectangular box with length x, breadth x², height x³.", a: "x⁶", opts: ["x⁵", "x⁶", "3x", "x³"] },
+            { q: "A man travels at (v+5) km/h for 2 hours. Distance covered?", a: "2v + 10", opts: ["v+7", "2v+10", "v+10", "None"] },
+            { q: "If the side of a square is increased by 3 units, area increases by?", a: "6s + 9", opts: ["9", "6s+9", "3", "None"] },
+            { q: "Cost of (x+y) kg of sugar at ₹(x-y) per kg is?", a: "x² - y²", opts: ["x² + y²", "x² - y²", "2x", "0"] },
+            { q: "Find total surface area of a cube with side 2x.", a: "24x²", opts: ["4x²", "8x²", "24x²", "None"] },
+            { q: "If a car covers (x² - 1) km in (x - 1) hours, its speed is?", a: "x + 1", opts: ["x", "x+1", "x-1", "1"] },
+            { q: "A room has dimensions (l+1) and (w+1). Area of floor?", a: "lw + l + w + 1", opts: ["lw+1", "l+w+1", "lw+l+w+1", "None"] },
+            { q: "Sum of a number and its reciprocal is 2. The number is?", a: "1", opts: ["0", "1", "2", "None"] }
+        ],
+        "Tough": [
+            { q: "Area of a circle with radius (r+1) is ____.", a: "π(r² + 2r + 1)", opts: ["πr² + 1", "π(r² + 2r + 1)", "2πr + 2π", "None"] },
+            { q: "If length of rectangle is (x+y) and breadth is (x-y), find its diagonal.", a: "√(2x² + 2y²)", opts: ["x", "y", "√(2x² + 2y²)", "None"] },
+            { q: "What is the area of a path 1m wide surrounding a square field of side 's'?", a: "4s + 4", opts: ["4s", "s² + 4", "4s + 4", "None"] },
+            { q: "Volume of a cylinder with radius 'r' and height 'r+h' is?", a: "πr³ + πr²h", opts: ["πr²h", "πr³ + πr²h", "2πr² + 2πrh", "None"] },
+            { q: "Find total cost: (2x+3) items at ₹(x+1) each and (x-1) items at ₹(2x-1) each.", a: "4x² + 3x + 4", opts: ["4x² + 3x + 4", "4x² + 5x + 2", "3x + 2", "None"] },
+            { q: "An equilateral triangle has side (a+b). Its perimeter is?", a: "3a + 3b", opts: ["a+b+3", "3a+3b", "3ab", "None"] },
+            { q: "A box has volume x³ - 1. If length is x-1 and breadth is 1, find height.", a: "x² + x + 1", opts: ["x+1", "x² + x + 1", "x² - 1", "None"] },
+            { q: "Difference between squares of two consecutive odd numbers (2n-1) and (2n+1).", a: "8n", opts: ["4n", "8n", "2", "None"] },
+            { q: "If interest is calculated on ₹(P+x) at rate r for time t. Interest = ?", a: "(P+x)rt/100", opts: ["Prt/100", "(P+x)rt/100", "xrt/100", "None"] },
+            { q: "A square of side 'x' has a circle of radius 'x/2' inside it. Remaining area?", a: "x²(1 - π/4)", opts: ["x² - πr²", "x²(1 - π/4)", "x²π", "None"] }
+        ]
+    }
+};
+
