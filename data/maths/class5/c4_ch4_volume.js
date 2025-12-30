8333b08c0783595a69ab52cbad1e650ab81f9bed
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 18:25:46 +0530

    Add volume questions for Chapter 4

---
+++ data/maths/class5/c4_ch4_volume.js
@@ -0,0 +1,41 @@
+window.currentChapterData = {
+    "Volume of Cuboids": {
+        "Easy": [
+            { q: "Volume = Length x Breadth x ____.", a: "Height", opts: ["Width", "Height", "Area", "Side"] },
+            { q: "Unit of volume is ____.", a: "Cubic units", opts: ["Square units", "Cubic units", "Meters", "Liters"] },
+            { q: "Volume of a cube with side 2cm?", a: "8 cm³", opts: ["4 cm³", "6 cm³", "8 cm³", "12 cm³"] },
+            { q: "Find volume: L=5, B=2, H=1.", a: "10", opts: ["7", "8", "10", "15"] },
+            { q: "Volume of a cube of side 1m?", a: "1 m³", opts: ["1 m³", "3 m³", "6 m³", "0"] },
+            { q: "How many dimensions does volume have?", a: "3", opts: ["1", "2", "3", "4"] },
+            { q: "Space occupied by a solid is its ____.", a: "Volume", opts: ["Area", "Perimeter", "Volume", "Weight"] },
+            { q: "Volume of a cuboid (L=10, B=1, H=1)?", a: "10", opts: ["10", "11", "12", "1"] },
+            { q: "If side of cube is 's', Volume = ?", a: "s x s x s", opts: ["s+s+s", "s x s", "s x s x s", "4s"] },
+            { q: "Standard unit of volume?", a: "Cubic Meter", opts: ["Meter", "Square Meter", "Cubic Meter", "Liter"] }
+        ],
+        "Moderate": [
+            { q: "Volume of cube with side 5cm?", a: "125 cm³", opts: ["25 cm³", "75 cm³", "125 cm³", "150 cm³"] },
+            { q: "Find H if V=60, L=5, B=4.", a: "3", opts: ["2", "3", "4", "5"] },
+            { q: "Volume of cuboid: 10cm x 5cm x 2cm?", a: "100 cm³", opts: ["17 cm³", "70 cm³", "100 cm³", "1000 cm³"] },
+            { q: "Which has more volume: Cube side 4 or Cuboid 5x4x3?", a: "Cube (64 > 60)", opts: ["Cube", "Cuboid", "Equal", "None"] },
+            { q: "Volume of a brick of 20cm x 10cm x 5cm?", a: "1000 cm³", opts: ["35 cm³", "1000 cm³", "2000 cm³", "500 cm³"] },
+            { q: "1 Cubic Meter = ____ Cubic Centimeters.", a: "10,00,000", opts: ["1,000", "10,000", "1,00,000", "10,00,000"] },
+            { q: "Side of cube whose volume is 27 cm³?", a: "3cm", opts: ["3cm", "9cm", "13.5cm", "27cm"] },
+            { q: "Area of base of cuboid is 20, H is 5. Volume?", a: "100", opts: ["25", "100", "120", "4"] },
+            { q: "A tank is 2m x 2m x 2m. Volume?", a: "8 m³", opts: ["4 m³", "6 m³", "8 m³", "12 m³"] },
+            { q: "If all sides of a cube are doubled, volume becomes?", a: "8 times", opts: ["Double", "4 times", "8 times", "16 times"] }
+        ],
+        "Tough": [
+            { q: "How many cubes of side 2cm fit in a box 10x8x6?", a: "60", opts: ["480", "240", "120", "60"] },
+            { q: "Volume of 1L water in cm³?", a: "1000 cm³", opts: ["100", "1000", "10000", "1"] },
+            { q: "Find surface area of a cube with side 3cm.", a: "54 cm²", opts: ["27", "54", "36", "18"] },
+            { q: "A wall is 4m x 3m x 0.2m. Volume?", a: "2.4 m³", opts: ["12 m³", "2.4 m³", "7.2 m³", "0.24 m³"] },
+            { q: "Water in a tank is 2m deep. Tank is 5m x 4m. Volume of water?", a: "40 m³", opts: ["20 m³", "40 m³", "10 m³", "80 m³"] },
+            { q: "Ratio of volumes of two cubes with sides 1:2?", a: "1:8", opts: ["1:2", "1:4", "1:8", "1:6"] },
+            { q: "Volume of a box is 240. If base is 6x5, height is?", a: "8", opts: ["6", "8", "10", "12"] },
+            { q: "How many 1cm cubes make 1 liter?", a: "1000", opts: ["100", "1000", "10000", "1"] },
+            { q: "Side of cube is 10cm. Volume in Liters?", a: "1L", opts: ["1L", "10L", "0.1L", "1000L"] },
+            { q: "Find volume of largest cube that fits in a 5x4x3 box.", a: "27", opts: ["64", "27", "8", "125"] }
+        ]
+    }
+};
+
