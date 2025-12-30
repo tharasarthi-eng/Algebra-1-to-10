954b7ffb1be46cbb9e3ff65ee32d34efbb1f76c0
tharasarthi-eng <tharasarthi@gmail.com>
2025-12-30 18:12:18 +0530

    Add c4_ch2_factors.js file

---
+++ data/maths/class4/c4_ch2_factors.js
@@ -0,0 +1,117 @@
+
+window.currentChapterData = {
+    "Factors and Multiples": {
+        "Easy": [
+            { q: "Multiples of 5 end with ___ or ___.", a: "0, 5", opts: ["0, 5", "1, 2", "5, 6", "Any"] },
+            { q: "Is 12 a multiple of 3?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 5 a factor of 21?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "First three multiples of 2:", a: "2, 4, 6", opts: ["1, 2, 3", "2, 4, 6", "2, 3, 4", "0, 2, 4"] },
+            { q: "Smallest factor of any number?", a: "1", opts: ["0", "1", "The number", "2"] },
+            { q: "Largest factor of 10?", a: "10", opts: ["1", "2", "5", "10"] },
+            { q: "How many factors does 7 have?", a: "2", opts: ["1", "2", "7", "0"] },
+            { q: "Every number is a multiple of ____.", a: "1", opts: ["0", "1", "2", "10"] },
+            { q: "Is 0 a factor of any number?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Common factor of all even numbers?", a: "2", opts: ["1", "2", "4", "0"] }
+        ],
+        "Moderate": [
+            { q: "Factors of 12 are:", a: "1, 2, 3, 4, 6, 12", opts: ["1, 2, 3, 4, 6, 12", "2, 3, 4, 6", "1, 12", "None"] },
+            { q: "First common multiple of 2 and 3?", a: "6", opts: ["1", "5", "6", "12"] },
+            { q: "Is 100 a multiple of 8?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Fourth multiple of 12?", a: "48", opts: ["12", "24", "36", "48"] },
+            { q: "How many factors does 16 have?", a: "5", opts: ["3", "4", "5", "6"] },
+            { q: "Find common factors of 8 and 12:", a: "1, 2, 4", opts: ["1, 2, 4", "1, 2, 4, 8", "1, 2", "None"] },
+            { q: "15 is a multiple of which prime number?", a: "3 and 5", opts: ["2", "3 and 5", "7", "11"] },
+            { q: "Multiples of a number are ____.", a: "Infinite", opts: ["Finite", "Infinite", "Only ten", "None"] },
+            { q: "Factors of a number are ____.", a: "Finite", opts: ["Finite", "Infinite", "Unknown", "None"] },
+            { q: "Is every multiple of 10 also a multiple of 5?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] }
+        ],
+        "Tough": [
+            { q: "Perfect numbers are equal to the sum of their factors (except itself). Which is perfect?", a: "6", opts: ["4", "6", "8", "10"] },
+            { q: "LCM of 4 and 6?", a: "12", opts: ["2", "12", "24", "6"] },
+            { q: "HCF of 12 and 18?", a: "6", opts: ["2", "3", "6", "36"] },
+            { q: "Which number is a factor of every even number but not every odd?", a: "2", opts: ["1", "2", "4", "0"] },
+            { q: "Number of factors of 100?", a: "9", opts: ["5", "7", "9", "10"] },
+            { q: "Is the number of multiples of 7 between 1 and 100 even or odd?", a: "Even (14)", opts: ["Even (14)", "Odd (13)", "Even (12)", "Odd (15)"] },
+            { q: "Can a number be a factor and multiple of itself?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Find the smallest number which is a multiple of 2, 3, and 5.", a: "30", opts: ["10", "15", "30", "60"] },
+            { q: "Sum of all factors of 10?", a: "18", opts: ["17", "18", "10", "15"] },
+            { q: "Which number has only one factor?", a: "1", opts: ["0", "1", "2", "Any prime"] }
+        ]
+    },
+    "Prime and Composite": {
+        "Easy": [
+            { q: "A number with only 2 factors is called ____.", a: "Prime", opts: ["Prime", "Composite", "Even", "Odd"] },
+            { q: "A number with more than 2 factors is ____.", a: "Composite", opts: ["Prime", "Composite", "Even", "Odd"] },
+            { q: "Is 1 a prime number?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Smallest prime number?", a: "2", opts: ["1", "2", "3", "0"] },
+            { q: "Which is prime: 2, 4, 6, 8?", a: "2", opts: ["2", "4", "6", "8"] },
+            { q: "Which is composite: 3, 5, 7, 9?", a: "9", opts: ["3", "5", "7", "9"] },
+            { q: "Smallest composite number?", a: "4", opts: ["1", "2", "4", "6"] },
+            { q: "Is 11 prime?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Every even number except 2 is ____.", a: "Composite", opts: ["Prime", "Composite", "Odd", "None"] },
+            { q: "How many factors does 13 have?", a: "2", opts: ["1", "2", "3", "13"] }
+        ],
+        "Moderate": [
+            { q: "Prime numbers between 1 and 10 are:", a: "2, 3, 5, 7", opts: ["1, 2, 3, 5", "2, 3, 5, 7", "3, 5, 7, 9", "2, 4, 6, 8"] },
+            { q: "Which is the only even prime number?", a: "2", opts: ["0", "2", "4", "None"] },
+            { q: "Are all odd numbers prime?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 21 prime or composite?", a: "Composite", opts: ["Prime", "Composite", "Neither", "Both"] },
+            { q: "Find the prime factors of 10:", a: "2, 5", opts: ["1, 2, 5, 10", "2, 5", "2, 2, 2", "5, 5"] },
+            { q: "Is 51 prime?", a: "No (17x3)", opts: ["Yes", "No (17x3)", "Maybe", "None"] },
+            { q: "Two prime numbers with a difference of 2 are called ____.", a: "Twin Primes", opts: ["Even Primes", "Twin Primes", "Co-primes", "Double Primes"] },
+            { q: "Is 1 composite?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "The number of prime numbers between 1 and 20 is:", a: "8", opts: ["7", "8", "9", "10"] },
+            { q: "Which prime number is just after 19?", a: "23", opts: ["20", "21", "22", "23"] }
+        ],
+        "Tough": [
+            { q: "Smallest odd composite number?", a: "9", opts: ["1", "3", "9", "15"] },
+            { q: "Are 8 and 9 co-prime?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Which prime number is a factor of all even numbers?", a: "2", opts: ["2", "3", "5", "None"] },
+            { q: "How many prime numbers between 90 and 100?", a: "1 (97)", opts: ["0", "1 (97)", "2", "3"] },
+            { q: "Prime factorization of 36:", a: "2x2x3x3", opts: ["4x9", "6x6", "2x2x3x3", "2x18"] },
+            { q: "Is 91 prime?", a: "No (13x7)", opts: ["Yes", "No (13x7)", "Maybe", "None"] },
+            { q: "Sum of first three prime numbers?", a: "10", opts: ["6", "10", "15", "9"] },
+            { q: "Which is NOT a twin prime pair?", a: "(7, 9)", opts: ["(3, 5)", "(5, 7)", "(7, 9)", "(11, 13)"] },
+            { q: "Largest prime number below 50?", a: "47", opts: ["43", "47", "49", "51"] },
+            { q: "Is the sum of two prime numbers always even?", a: "No (2+3=5)", opts: ["Yes", "No (2+3=5)", "Maybe", "None"] }
+        ]
+    },
+    "Divisibility Rules": {
+        "Easy": [
+            { q: "A number is divisible by 2 if it ends in ____.", a: "0, 2, 4, 6, 8", opts: ["0, 5", "0, 2, 4, 6, 8", "Any odd", "None"] },
+            { q: "Divisibility rule for 5: Last digit is ____.", a: "0 or 5", opts: ["0", "5", "0 or 5", "Even"] },
+            { q: "Rule for 10: Last digit must be ____.", a: "0", opts: ["0", "5", "0 or 5", "Even"] },
+            { q: "Is 45 divisible by 5?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 22 divisible by 2?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 100 divisible by 10?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 13 divisible by 2?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Numbers divisible by 2 are called ____.", a: "Even", opts: ["Even", "Odd", "Prime", "None"] },
+            { q: "Which is divisible by 5: 12, 24, 35?", a: "35", opts: ["12", "24", "35", "None"] },
+            { q: "Which is divisible by 10: 55, 60, 65?", a: "60", opts: ["55", "60", "65", "None"] }
+        ],
+        "Moderate": [
+            { q: "Rule for 3: Sum of digits must be divisible by ____.", a: "3", opts: ["1", "2", "3", "9"] },
+            { q: "Is 123 divisible by 3?", a: "Yes (1+2+3=6)", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Rule for 6: Must be divisible by both ____ and ____.", a: "2 and 3", opts: ["2 and 3", "3 and 4", "5 and 1", "None"] },
+            { q: "Rule for 9: Sum of digits must be divisible by ____.", a: "9", opts: ["3", "6", "9", "1"] },
+            { q: "Is 81 divisible by 9?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 456 divisible by 2 and 3?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Which is divisible by 3: 41, 52, 63?", a: "63", opts: ["41", "52", "63", "None"] },
+            { q: "Is 505 divisible by 10?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 1,002 divisible by 3?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Which is divisible by 9: 18, 28, 38?", a: "18", opts: ["18", "28", "38", "None"] }
+        ],
+        "Tough": [
+            { q: "Rule for 4: Last ____ digits must be divisible by 4.", a: "Two", opts: ["One", "Two", "Three", "All"] },
+            { q: "Is 512 divisible by 4?", a: "Yes (12/4=3)", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 1,234,560 divisible by 5 and 10?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Which is divisible by 4: 114, 224, 334?", a: "224", opts: ["114", "224", "334", "None"] },
+            { q: "Is 999 divisible by 9?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "A number divisible by 9 is always divisible by ____.", a: "3", opts: ["2", "3", "6", "10"] },
+            { q: "A number divisible by 3 is always divisible by 9?", a: "No", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Is 1,111,111 divisible by 3?", a: "No (Sum is 7)", opts: ["Yes", "No", "Maybe", "None"] },
+            { q: "Find the smallest 3-digit number divisible by 2, 3, and 5.", a: "120", opts: ["110", "120", "150", "100"] },
+            { q: "Is 1,000 divisible by 4?", a: "Yes", opts: ["Yes", "No", "Maybe", "None"] }
+        ]
+    }
+};
